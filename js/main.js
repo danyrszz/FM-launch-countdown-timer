77e18c029f8c9bcb726ff8a4d562a6c594b102c5
Daniel Ramírez <daniel_ramirezs@hotmail.com>
2022-04-02 12:13:04 -0600

    final version responsive

--- js/main.js
+++ js/main.js
@@ -7,32 +7,3 @@ const daysLbl = document.querySelector("#days");
 
 const timer = new Time(secondsLbl, minutesLbl, hoursLbl, daysLbl);
 timer.start();
-
-
-
-
-
-
-// const top = document.querySelector(".top");
-// const bottom = document.querySelector(".bottom");
-
-// const topBack = document.querySelector(".top-back");
-// const bottomBack = document.querySelector(".bottom-back");
-
-// setInterval(() => {
-
-//     let flag = false;
-//     top.style.transform = "perspective(500px) rotatex(-90deg)";
-//     bottom.style.transform = "perspective(500px) rotatex(-90deg)";
-//     bottom.style.transition = "transform .2s linear 0.2s"
-//     top.style.transition = "transform .2s linear"
-//     let s = setInterval(() => {
-//         flag=true;
-//         top.style.transform = "perspective(500px) rotatex(0deg)";
-//         top.style.transition = "transform 0s "
-//         bottom.style.transform = "perspective(500px) rotatex(0deg)";
-//         bottom.style.transition = "transform 0s "
-//         if(flag){ clearInterval(s); }
-//     }, 800);
-
-// }, 1000);
